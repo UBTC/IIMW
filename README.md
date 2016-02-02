37cfb800154a20b4d3e5294968b8d51379ded9ca
mogeiwang <mogeiwang@gmail.com>
2016-02-03 04:28:29 +0800

    Change to Kwin

--- README.md
+++ README.md
@@ -7,18 +7,14 @@ Languages
 - [wipy](https://github.com/ubtc/wipy) for setting up [ipython](http://ipython.org/)
 
 Win manager
+- setting up [kwin window manager](http://github.com/KDE/kwin) by default
 - [boxer](https://github.com/ubtc/boxer) for setting up [openbox window manager](http://openbox.org/wiki/Main_Page)
 - [monadic](https://github.com/ubtc/monadic) for setting up [xmonad window manager](http://xmonad.org/)
-
-Dock
-- zero configuration for [plank](http://wiki.go-docky.com/index.php?title=Plank:Introduction)
 
 Text editor
 - [vine](https://github.com/ubtc/vine) for setting up [neovim editor](https://neovim.io/)
 - [vpEMacs](https://github.com/ubtc/vpEmacs) for setting up [emacs editor](http://www.gnu.org/software/emacs/)
 
 Others applications are mostly [Xubuntu](https://www.xubuntu.org), and may switch to [Lubuntu](http://lubuntu.net/) later.
-
-NOTE: scripts in the subdirs are merely for backup. They are `NOT` used in setting up.
 
 M.W.
--- install.sh
+++ install.sh
@@ -5,20 +5,19 @@ echo "installing"
 nano /etc/apt/sources.list
 sudo apt-get update -y
 sudo apt-get upgrade -y
-# PPA may be required by neovim and plank:
-sudo apt-get install neovim firefox ipython obconf goldendict alsamixergui \
+# PPA may be required by neovim:
+sudo apt-get install firefox ipython goldendict alsamixergui python-igraph \
+  wireshark catfish jabref xbacklight xarchiver alsa-utils python-networkx \
   ufw suckless-tools xautolock hamster-indicator emacs24 fish tor synaptic \
-  wireshark catfish jabref xbacklight fcitx xarchiver alsa-utils evince sl \
   openssl openssh-client pandoc playonlinux xchm texlive-full wget gdb git \
   transmission-gtk zip pyflakes gtkorphan python-chaco gtk-recordmydesktop \
   skype unzip libav-tools john nmap kismet hydra ophcrack hunt aircrack-ng \
   meld rar unrar aria2 axel octave vidalia dmsetup cryptsetup libpam-mount \
-  plank gdebi auctex xfce4 clamav aspell exuberant-ctags python-setuptools \
-  gksu python-sklearn python-matplotlib python-sympy python-pandas openbox \
+  docky gdebi auctex xfce4 clamav aspell exuberant-ctags python-setuptools \
+  gksu python-sklearn python-matplotlib python-sympy python-pandas dolphin \
   vym golang python-scipy python-scientific python-pygraphviz pep8 calibre \
   amule gimp gmchess python-simpy gnupg gnuplot openvpn python-statsmodels \
-  python-networkx python-igraph python-yaml python-scitools xmonad mdpress \
-  python-regex yi tint2
+  python-yaml python-scitools python-regex evince mdpress okular kwin nvim \
 echo
 echo ">>> Installing all packages recommended by  python-scitools  is a good idea!!!"
 sudo apt-get autoremove --purge -y
@@ -46,24 +45,20 @@ git config --global push.default simple
 git config --global user.name mogeiwang
 git config --global user.email mogeiwang@gmail.com
 
-echo "configuring"
-mv ~/.config/openbox ~/.config/openbox.backup
-git clone 'https://github.com/ubtc/boxer' ~/.config/openbox
-# ---
-mv ~/.xmonad ~/.xmonad.backup
-git clone '(https://github.com/ubtc/monadic' ~/.xmonad
-xmonad --recompile
-# ---
-mv ~/.config/tint2 ~/.config/tint2.backup
-mkdir -p ~/.config/tint2
-cp ./wm/tint2rc ~/.config/tint2/
-# ---
+echo "Languages"
 mv ~/.config/ipython ~/.config/ipython.backup
 git clone 'https://github.com/ubtc/wipy' ~/.config/ipython
 # ---
 mv ~/.config/fish ~/.config/fish.backup
 git clone 'https://github.com/ubtc/goFish' ~/.config/fish
 sh ~/.config/fish/instpkg.sh
+
+echo "windows"
+mv ~/.kde/share/config/kwinrc ~/.kde/share/config/kwinrc.backup
+cp ./wm/kwinrc ~/.kde/share/config/kwinrc
+cp ./wm/_keylaunchrc ~/.keylaunchrc
+sudo cp ./wm/IIMW.desktop /usr/share/xsessions/
+sudo cp ./wm/startIIMW /usr/bin/
 
 echo "editors"
 mv ~/.emacs.d ~/.emacs.d.backup
