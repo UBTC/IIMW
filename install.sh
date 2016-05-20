f14a4bf692b30aa3a9396099d0adc6f161ae0954
mogeiwang <mogeiwang@gmail.com>
2016-05-20 23:46:34 +0800

    Switch to fluxbox

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ cat /etc/apt/sources.list # vim /etc/apt/sources.list
 sudo apt-get update -y
 
 sudo apt-get --allow-unauthenticated -y install \
-  xfce4 goldendict alsamixergui vym golang calibre openvpn openbox mdpress \
+  xfce4 goldendict alsamixergui vym golang calibre openvpn fluxbox mdpress \
   wireshark catfish jabref xbacklight xarchiver alsa-utils pep8 zathura sl \
   ufw xautolock hamster-indicator tmux tor synaptic zsh julia fish gnuplot \
   openssl openssh-client pandoc playonlinux xchm texlive-full wget gdb git \
@@ -83,8 +83,8 @@ mv ~/.juliarc.jl ~/.julia.backup.jl
 cp ~/.goJulia/_juliarc.jl ~/.juliarc.jl
 
 echo "Desktop"
-mv ~/.config/openbox ~/.config/openbox.backup
-git clone "https://github.com/UBTC/boxer" ~/.config/openbox
+mv ~/.fluxbox ~/.fluxbox.backup
+git clone "https://github.com/UBTC/bFox" ~/.fluxbox
 
 echo "Editors"
 mv ~/.emacs.d ~/.emacs.d.backup
