0a7562ca4bd8c980449e00a78c38dac2061564e8
M W <mogeiwang@gmail.com>
2017-05-25 16:50:45 -0400

    add Jupyter C/S alias, and do clearance

--- shell/config.fish
+++ shell/config.fish
@@ -2,8 +2,8 @@
 
 # Generic
 set fish_greeting (fortune)
-set EDITOR vim
-set VISUAL vim
+set EDITOR emacs
+set VISUAL emacs
 # ---
 set -x LANG en_US.UTF-8
 set -x LANGUAGE $LANG
@@ -17,9 +17,8 @@ set -x GOBIN $GOPATH/bin
 set -x PATH /usr/local/sbin $PATH
 set -x PATH $PATH $GOBIN
 set -U fish_user_paths $fish_user_paths $GOBIN
-set -x JULIAFUNCDIR ~/julia/juliaFunc
-# set -x JULIA_PKGDIR /usr/local/julia/julia-packages
-set -gx PATH ~/tensorflow/bin $PATH
+set -gx PATH $HOME/anaconda/bin $PATH
+set -gx PATH $HOME/tensorflow/bin $PATH
 set -x NVIM_LISTEN_ADDRESS /tmp/neovim/neovim
 
 # Aliases
@@ -30,21 +29,19 @@ alias ,,,,='cd ../../../..'
 alias ,,,,,='cd ../../../../..'
 alias -='cd -'
 alias ed='emacs -nw'
+alias em='emacs'
 alias vi='vim'
-#alias jl='julia'
 alias pp='python3'
 alias py='ipython3'
 alias pyss='python -m SimpleHTTPServer'
 alias jpt='jupyter'
 alias jpn='jupyter notebook'
-alias jpc='jupyter nbconvert --to'
+alias jps='jupyter notebook --no-browser --port=8889' # server
+alias jpc='ssh -N -f -L localhost:8888:localhost:8889 username@remote_host_name' # fill real info here!!!
+alias jpcvt='jupyter nbconvert --to'
 alias mm='tmux -2 attach'
-alias sc='scala -feature'
-alias scc='scalac'
 alias sskg='ssh-keygen'
-# use sskg to generate id_rsa&id_ras.pub,
-# and copy .pub to ~/.ssh/authorized_keys on server.
-#alias nv="stterm -c Neovim -T Neovim -f 'Liberation Mono:pixelsize=15:antialias=true:autohint=true' -e fish -c 'nvim'"
+# use sskg to generate id_rsa&id_ras.pub, and copy .pub to ~/.ssh/authorized_keys on server.
 alias mc="pandoc -f markdown+lhs slides.md -o slides.html -t dzslides -i -s -S --toc"
 alias gb="go build -compiler gccgo -gccgoflags='-O3' "
 alias uu="sudo apt-get update -y; sudo apt-get upgrade -y; sudo apt-get autoremove --purge -y"
@@ -65,9 +62,7 @@ function lsp
 end
 
 # Misc
-# initialise_ssh_agent
 stty echo
-# vi_mode
 
 # Local configuration
 if test -r ~/.config/fish/local.fish
